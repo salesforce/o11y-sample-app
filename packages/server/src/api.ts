20cd4bd69e9d62bec3ed3d4ae38098666c5c47c4
Levent Oz <loz@salesforce.com>
2021-12-30 16:39:29 -0800

    Add ability to serve from web from the API server

--- packages/server/src/api.ts
+++ packages/server/src/api.ts
@@ -2,33 +2,43 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
+import { URL } from 'url';
 import { processCoreEnvelope } from './coreEnvelope';
 import { processHeaders } from './headers';
 
 const HOST = process.env.API_HOST || 'localhost';
 const PORT = process.env.API_PORT || 3002;
-const LOG_HEADERS = false;
+const SERVE_WEB = process.env.O11Y_SERVE_WEB === 'true' || false;
+const LOG_HEADERS = process.env.O11Y_LOG_HEADERS === 'true' || false;
+const __dirname = new URL('.', import.meta.url).pathname;
+const DIST_DIR = path.resolve(__dirname, '..', 'dist-client');
 
-express()
-    .use(cors())
-    // .use(bodyParser.urlencoded({
-    //     extended: true
-    // }))
-    // .use(express.json({
-    //     extended: true,
-    //     inflate: true,
-    //     limit: '100kb',
-    //     parameterLimit: 1000,
-    //     strict: false,
-    //     //type: 'application/x-www-form-urlencoded',
-    //     type: 'application/json',
-    //     verify: undefined
-    // }))
-    .use(
-        bodyParser.json({
-            type: 'application/json'
-        })
-    )
+const app = express();
+
+if (!SERVE_WEB) {
+    app.use(cors());
+}
+
+// .use(bodyParser.urlencoded({
+//     extended: true
+// }))
+// .use(express.json({
+//     extended: true,
+//     inflate: true,
+//     limit: '100kb',
+//     parameterLimit: 1000,
+//     strict: false,
+//     //type: 'application/x-www-form-urlencoded',
+//     type: 'application/json',
+//     verify: undefined
+// }))
+
+app.use(
+    bodyParser.json({
+        type: 'application/json'
+    })
+)
     .use(
         bodyParser.raw({
             type: 'application/octet-stream'
@@ -57,7 +67,15 @@ express()
         console.log('---------------------------------------------------------');
         console.log('Received call to /api/isodate');
         res.send(new Date().toISOString());
-    })
-    .listen(PORT, () =>
-        console.log(`✅  API Server started: http://${HOST}:${PORT}/api/uitelemetry`)
-    );
+    });
+
+if (SERVE_WEB) {
+    app.use(express.static(DIST_DIR));
+}
+
+app.listen(PORT, () => {
+    if (SERVE_WEB) {
+        console.log(`✅  Web Server started: http://${HOST}:${PORT}`);
+    }
+    console.log(`✅  API Server started: http://${HOST}:${PORT}/api/uitelemetry`);
+});
