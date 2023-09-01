7c9deb937f40c9ac1473e480851437be4129e62c
g-pezanoskicohen <g.pezanoskicohen@salesforce.com>
2023-09-01 16:51:43 -0500

    Add Dynamic Payload Providers example

--- packages/client/src/appPayloadProvider.ts
+++ packages/client/src/appPayloadProvider.ts
@@ -1,4 +1,4 @@
-import { SchematizedPayload } from 'o11y/dist/modules/o11y/client/interfaces';
+import { PayloadProviderArgs, SchematizedPayload } from 'o11y/dist/modules/o11y/client/interfaces';
 import { appPayloadSchema } from 'o11y_schema/sf_o11ySample';
 
 export class AppPayloadProvider {
@@ -24,12 +24,19 @@ export class AppPayloadProvider {
         });
     }
 
-    getPayload(): SchematizedPayload {
+    // getPayload can take an optional input of PayloadProviderArgs. Below is an example
+    // of using an argument from one of the fields. Note that the appPayload is created
+    // after the pagePayload, so the app has access to the pagePayload, but not vice-versa.
+    getPayload(args: PayloadProviderArgs): SchematizedPayload {
         return {
             schema: appPayloadSchema,
             payload: {
                 language: navigator.language,
-                frameRate: this._lastFrameRate
+                frameRate: this._lastFrameRate,
+                clientSessionId: args.logMeta.clientSessionId,
+
+                // We can access the pagePayload from the appPayloadProvider
+                url: args.logMeta.pagePayload.payload.url
             }
         };
     }
