984e30172bce7262365b80d66de429d3e7696bfe
Levent Oz <leventoz.dev@gmail.com>
2023-10-12 18:13:22 -0700

    add json viewer to utility

--- packages/client/src/modules/my/utilityPlay/utilityPlay.ts
+++ packages/client/src/modules/my/utilityPlay/utilityPlay.ts
@@ -7,10 +7,13 @@ import { base64BinToCSV } from '../../../../../_common/src/arrayUtil';
 
 export default class UtilityPlay extends LightningElement {
     private readonly _instr: Instrumentation;
+    private jv: HTMLElement;
 
     @track base64Text = '';
     @track csvText = '';
     @track lastResponse: string;
+    @track showJsonResponse = true;
+    @track expandAll = true;
     @track errorMsg: string;
     @track
     readonly uploadEndpointOptions: { label: string; value: string }[] = [
@@ -30,9 +33,10 @@ export default class UtilityPlay extends LightningElement {
         this._instr = getInstrumentation('Utility');
     }
 
-    @track uploadEndpoint: string = endpoints.sampleCsvEndpointWithTextReturn;
+    @track uploadEndpoint: string = endpoints.sampleCsvEndpointWithJsonReturn;
     handleUploadEndpointChange(event: CustomEvent): void {
         this.uploadEndpoint = event.detail.value;
+        this.showJsonResponse = this.uploadEndpoint === endpoints.sampleCsvEndpointWithJsonReturn;
     }
 
     handleBase64TextChange(event: CustomEvent) {
@@ -66,9 +70,49 @@ export default class UtilityPlay extends LightningElement {
                 body: this.csvText
             });
             this.lastResponse = await resp.text();
+
+            this.jv = document.createElement('json-viewer');
+            this.jv.setAttribute('data', this.lastResponse);
+
+            this._useJsonViewer(this.showJsonResponse);
         } catch (err) {
             this._instr.error(err as Error);
             this.lastResponse = err?.toString() ?? '<ERROR>';
         }
+    }
+
+    private _useJsonViewer(show: boolean): void {
+        const div = this.template.querySelector('.json-viewer-host') as HTMLDivElement;
+        if (div) {
+            if (show) {
+                if (this.jv) {
+                    if (div.firstChild) {
+                        div.replaceChild(this.jv, div.firstChild);
+                    } else {
+                        div.appendChild(this.jv);
+                    }
+                }
+            } else {
+                if (div.firstChild) {
+                    div.removeChild(div.firstChild);
+                }
+            }
+        }
+    }
+
+    handleToggleClick(): void {
+        this.showJsonResponse = !this.showJsonResponse;
+    }
+
+    renderedCallback(): void {
+        this._useJsonViewer(this.showJsonResponse);
+    }
+
+    handleExpandAllClick(): void {
+        (this.jv as any)?.expandAll();
+    }
+
+    handleCollapseAllClick(): void {
+        (this.jv as any)?.collapseAll();
     }
 }
